3ab28900d8abb6d44c6167a56cd17238b8c1df92
Rohit Verma <rv.work.rv@gmail.com>
2024-08-12 01:53:45 +0530

    drf docu

--- app/core/models.py
+++ app/core/models.py
@@ -11,8 +11,19 @@ class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_field):
         """Create, save and return a new User"""
+        if not email:
+            raise ValueError('User mush have an email address')
         user = self.model(email=self.normalize_email(email), **extra_field)
         user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password):
+        """Create and return a new Superuser"""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
         user.save(using=self._db)
 
         return user
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -30,3 +30,19 @@ class ModelTests(TestCase):
         for email, expected in sample_emails:
             user = get_user_model().objects.create_user(email, 'sample123')
             self.assertEqual(user.email, expected)
+
+
+    def test_new_user_email_raises_erro(self):
+        """Test that creating a user without an email a value Error"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user('','test123')
+
+    def test_create_superuser(self):
+        """Test creating a superuser"""
+        user = get_user_model().objects.create_superuser(
+            'test@example.com',
+            'test123',
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)

8746ab6fb0a1afaad443e92287c2d950b742df5b
Rohit Verma <rv.work.rv@gmail.com>
2024-08-13 23:56:12 +0530

    user

---
+++ app/user/views.py
@@ -0,0 +1,8 @@
+# Views for users API.
+
+from rest_framework import generics
+from user.serializers import UserSerializer
+
+class CreateUserView(generics.CreateAPIView):
+    #Create a new User in the system
+    serializer_class = UserSerializer
